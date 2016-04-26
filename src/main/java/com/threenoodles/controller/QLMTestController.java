8306d05a1f2a7d876e0b2f886ad55ef70bcb0835
wangxiaoman <283468598@qq.com>
2016-04-26 18:41:57 +0800

    【修改】千里码习题

--- src/main/java/com/threenoodles/controller/QLMTestController.java
+++ src/main/java/com/threenoodles/controller/QLMTestController.java
@@ -2,16 +2,19 @@ package com.threenoodles.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/qlm")
+@RequestMapping("/train")
 public class QLMTestController {
   
-  @RequestMapping(value = "/index", produces = { "text/plain;charset=UTF-8" })
+  @RequestMapping(value = "/plus", produces = { "text/plain;charset=UTF-8" })
   @ResponseBody
-  public String index() {
-    String name = new String("simmer");
-    return name;
+  public String index(
+      @RequestParam(required=true,value="a") int a,
+      @RequestParam(required=true,value="b") int b
+      ) {
+    return String.valueOf(a+b);
   }
 }
